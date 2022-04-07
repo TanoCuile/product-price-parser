33f2b196dfabf3e1287eb27375ebbdf89eaa7278
Serhii Shvets <sshvets@shutterstock.com>
2022-04-07 22:04:59 +0300

    Async/Await example

--- cli.js
+++ cli.js
@@ -2,47 +2,6 @@ const inquirer = require("inquirer");
 const helpers = require("./helpers");
 const { getProducts } = require("./getProducts");
 
-function askUserForProduct(products) {
-  return (
-    inquirer
-      .prompt([
-        // Згідно API https://www.npmjs.com/package/inquirer передали список запитань
-        {
-          message: "Please insert product name",
-          name: "product_name", // Унікальний ключ запитання
-          type: "input", // Тип очікуваної відповіді
-        },
-      ])
-      // Promise API(then/catch) - для того щоб опрацювати дію, час закінчення якої невідомий
-      .then(
-        // Передаємо функцію обробник відповіді користувача
-        function (answers) {
-          console.log("Виші відповіді: ", answers);
-
-          // instanceof Array
-          const intrestingProduct = products.find(
-            (product) => product.product === answers.product_name
-          );
-
-          console.log("Продукт знайдено!");
-          console.log("Остання ціна\t| Стан");
-          console.log(
-            `${getLastPrice(intrestingProduct)}\t\t| ${getFlagRepresentation(
-              intrestingProduct
-            )}`
-          );
-        }
-      )
-      .catch(
-        // Передаємо функцію обробник помилки
-        function (error) {
-          console.error("Підчас обробки виникла помилка: ", error);
-        }
-      )
-  );
-}
-
-askUserForProduct(getProducts());
 function getLastPrice(intrestingProduct) {
   return intrestingProduct.prices[intrestingProduct.prices.length - 1];
 }
@@ -53,3 +12,35 @@ function getFlagRepresentation(intrestingProduct) {
     intrestingProduct.prices[intrestingProduct.prices.length - 2]
   );
 }
+
+async function askUserForProduct(products) {
+  try {
+    const answers = await inquirer.prompt([
+      // Згідно API https://www.npmjs.com/package/inquirer передали список запитань
+      {
+        message: "Please insert product name",
+        name: "product_name", // Унікальний ключ запитання
+        type: "input", // Тип очікуваної відповіді
+      },
+    ]);
+    
+    console.log("Виші відповіді: ", answers);
+
+    // instanceof Array
+    const intrestingProduct = products.find(
+      (product) => product.product === answers.product_name
+    );
+
+    console.log("Продукт знайдено!");
+    console.log("Остання ціна\t| Стан");
+    console.log(
+      `${getLastPrice(intrestingProduct)}\t\t| ${getFlagRepresentation(
+        intrestingProduct
+      )}`
+    );
+  } catch (error) {
+    console.error("Підчас обробки виникла помилка: ", error);
+  }
+}
+
+askUserForProduct(getProducts());
---
+++ timeout.js
@@ -0,0 +1,78 @@
+function loadPageData(product) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // AFTER SOME AMOUNT OF TIME...
+      resolve({ body: `LOREM ipsum dolor\nset amet comit\ndolor.` });
+    }, 1000);
+  });
+}
+
+// callback hell
+// Promise hell
+
+loadPageData()
+  .then(({ body }) => {
+    return body.split("\n");
+  })
+  .catch((error) => {
+    console.error(error);
+    return [];
+  })
+  .then((lines) => {
+    if (lines.length > 1) {
+      // Do somethin
+    } else if (lines.length > 2) {
+      // Do somethin
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const products = require("./products.json");
+
+// Promise.all(products.map((p) => loadPageData(p))).then(() => ...);
+
+products.reduce(
+  (prev, p) =>
+    prev.then((combined) =>
+      loadPageData(p)
+        .then(({ body }) => {
+          return body.split("\n");
+        })
+        .catch((error) => {
+          console.error(error);
+          return [];
+        })
+        .then((lines) => {
+          combined.push({ product: p, lines: lines });
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    ),
+  Promise.resolve([])
+);
+
+// Async/Await
+
+async function refreshPrices() {
+  const combined = [];
+  try {
+    for (const p of products) {
+      let lines = [];
+      try {
+        const { body } = await loadPageData(p);
+        lines = body.split("\n");
+      } catch (error) {
+        console.log(error);
+        lines = [];
+      }
+      combined.push({ product: p, lines: lines });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+refreshPrices();
