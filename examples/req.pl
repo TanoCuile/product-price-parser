2900a15dcbf6aabe7c2622e4b68b60de8a0b3760
Serhii Shvets <sshvets@shutterstock.com>
2022-04-11 19:04:57 +0300

    Example request

---
+++ examples/req.pl
@@ -0,0 +1,26 @@
+# Request headers
+Method: POST
+Request URL: https://allo.ua/API/login
+accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif
+accept-encoding: gzip, deflate, br
+accept-language: en-US,en;q=0.9
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36
+# Rest headers
+sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"
+sec-fetch-dest: document
+sec-fetch-mode: navigate
+sec-fetch-site: none
+sec-fetch-user: ?1
+upgrade-insecure-requests: 1
+content-length: 2324545
+authorization: Barier dkjfj;sdfjdsjf;lksdjf;sdklfj;sdlkjflsjdf;kjdskjfhdjghshdfgdfkg
+# Cookie heasder
+cookie: session=sdsdjksjdlsjdkalskd Marc 31; store=default_ua; __uzma=9ed20b8d-d63c-4314-8e16-be03f2d68852; __uzmb=1649682555; __uzme=9455; frontend=7f77a40e7095409588194a933909dec0; is_bot=0; detect_mobile_type=0; _gcl_au=1.1.1495916285.1649682557; __utma=45757819.1978896667.1649682557.1649682557.1649682557.1; __utmc=45757819; __utmz=45757819.1649682557.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=45757819.1.10.1649682557; _ga=GA1.2.1978896667.1649682557; _gid=GA1.2.1316377513.1649682557; __ssds=2; sc=3E1BFC09-DA34-58E4-C390-F10E57D2F044; __uzmcj2=213691071469; __uzmdj2=1649682557; __uzmbj2=1649682557; __uzmaj2=72ec15f4-9e7d-4182-854c-99fb4aeb859a; __ssuzjsr2=a9be0cd8e;
+# Request body
+{"user_data": {"user_name": "VASIA", "pass": "124343"}}
+
+
+# Server header
+keep-alive: true # HTTP 1.1 змушує клієнта доооовго чекати закінчення відповіді (чати, живі новини, все на loong-polling)
